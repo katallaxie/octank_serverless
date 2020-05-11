b77a1bc066c37f33840ecb101a260d0eab7f9747
Sebastian Döll <sebastian@katallaxie.me>
2020-05-11 13:31:06 +0200

    (feat) adding dynamic header

--- packages/app/src/components/header/index.tsx
+++ packages/app/src/components/header/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import { Heading, Pane } from 'evergreen-ui'
+import awsmobile from '../../aws-exports'
 
 export function Header() {
   return (
     <Pane borderBottom display="flex" padding={22}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Octank Videos</title>
+        <link rel="canonical" href={`https://${awsmobile}`} />
+      </Helmet>
       <Heading size={800}>Octank Videos</Heading>
     </Pane>
   )
